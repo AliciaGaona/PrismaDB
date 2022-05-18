11bc0ae10cd74650612106edc31c34262d9bffc8
AliciaGaona <aligaovela@gmail.com>
2022-05-17 22:57:17 -0500

    Automatizar inserciones de datos

---
+++ prisma/seed.js
@@ -0,0 +1,53 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+(async function main() {
+  try {
+    const woopa = await prisma.explorer.upsert({
+      where: { name: 'Woopa' },
+      update: {},
+      create: {
+        name: 'Woopa',
+				username: 'ajolonauta',
+				mission: 'Node'
+      },
+    });
+
+    const woopa1 = await prisma.explorer.upsert({
+      where: { name: 'Woopa1' },
+      update: {},
+      create: {
+        name: 'Woopa1',
+				username: 'ajolonauta1',
+				mission: 'Node'
+      },
+    });
+
+    const woopa2 = await prisma.explorer.upsert({
+      where: { name: 'Woopa 2' },
+      update: {},
+      create: {
+        name: 'Woopa 2',
+				username: 'ajolonauta2',
+				mission: 'Java'
+      },
+    });
+
+    const woopa3 = await prisma.explorer.upsert({
+      where: { name: 'Woopa 3' },
+      update: {},
+      create: {
+        name: 'Woopa 3',
+				username: 'ajolonauta3',
+				mission: 'Node'
+      },
+    });
+
+    console.log('Create 3 explorers');
+  } catch(e) {
+    console.error(e);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
+})();
