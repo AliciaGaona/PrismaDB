db6d0b01930cfb4ce7e50454d1a7badfe1c6084f
AliciaGaona <aligaovela@gmail.com>
2022-05-18 13:45:50 -0500

    Agregando servicio get explorers

--- app/server.js
+++ app/server.js
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
   res.json({message: 'alive'});
 });
 
+app.get('/explorers', async (req, res) => {
+  const allExplorers =  await prisma.explorer.findMany({});
+  res.json(allExplorers);
+});
+
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
