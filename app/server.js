170433a52e31fa3bcf0819d99aac34761a0d89bb
AliciaGaona <aligaovela@gmail.com>
2022-05-19 00:31:51 -0500

    Se agrega endpoint Delete para borrar el explorer según

--- app/server.js
+++ app/server.js
@@ -48,6 +48,11 @@ app.put('/explorers/:id', async (req, res) => {
 	return res.json({message: "Actualizado correctamente"});
 });
 
+app.delete('/explorers/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+	await prisma.explorer.delete({where: {id: id}});
+	return res.json({message: "Eliminado correctamente"});
+});
 
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
