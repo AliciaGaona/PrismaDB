5f687adb1bca4726d61302bb0ba29f0e01e73c12
AliciaGaona <aligaovela@gmail.com>
2022-06-01 23:47:13 -0500

    Se agrega metodo delete

--- app/server.js
+++ app/server.js
@@ -155,7 +155,11 @@ app.put('/missionCommander/:id', async (req, res) => {
 	return res.json({message: "Actualizado correctamente"});
 });
 
-
+app.delete('/missionCommander/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+	await prisma.missionCommander.delete({where: {id: id}});
+	return res.json({message: "Eliminado correctamente"});
+});
 
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
